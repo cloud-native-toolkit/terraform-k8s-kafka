5e7af9c1d3ebdea16fff21515cce554e60d964bb
Sean Sundberg <seansund@us.ibm.com>
2020-07-10 15:46:45 -0500

    Disables wait for CSV delete in destroy

--- scripts/destroy-subscription.sh
+++ scripts/destroy-subscription.sh
@@ -15,15 +15,16 @@ echo "Removing kafka operator from ${OPERATOR_NAMESPACE} namespace"
 kubectl delete -f ${YAML_FILE} -n "${OPERATOR_NAMESPACE}"
 
 set +e
-
-sleep 2
-count=0
-while kubectl get csv -n "${OPERATOR_NAMESPACE}" | grep -q strimzi-cluster-operator; do
-  if [[ $count -eq 10 ]]; then
-    echo "Timed out waiting for Kafka CSV to be deleted from ${OPERATOR_NAMESPACE}"
-    exit 1
-  fi
-
-  echo "Waiting for Kafka CSV to be deleted from ${OPERATOR_NAMESPACE}"
-  sleep 15
-done
+#
+#sleep 2
+#count=0
+#while kubectl get csv -n "${OPERATOR_NAMESPACE}" | grep -q strimzi-cluster-operator; do
+#  if [[ $count -eq 10 ]]; then
+#    echo "Timed out waiting for Kafka CSV to be deleted from ${OPERATOR_NAMESPACE}"
+#    exit 1
+#  fi
+#
+#  echo "Waiting for Kafka CSV to be deleted from ${OPERATOR_NAMESPACE}"
+#  sleep 15
+#  count=$((count+1))
+#done
